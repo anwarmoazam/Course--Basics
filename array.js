1d92ab6e1249eb58983e9b517ee012a68ab34b8a
anwar_moazam <anwar.palimarwar@gmail.com>
2021-10-03 01:12:23 +0530

    coding session with moien sir

--- array.js
+++ array.js
@@ -186,7 +186,7 @@ function twoSum(arr,target){
     }
 }
 console.log(twoSum([2,7,11,15],9));
-console.log(twoSum([9,7,11,15],22eE));
+console.log(twoSum([9,7,11,15],22));
 console.log(twoSum([3,2,1],4));
 console.log(twoSum([3,3],6));
 
