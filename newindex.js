759cc25698be71f2578053bbcf51b2a8ffffeee5
unknown <you@example.com>
2021-10-08 18:19:40 +0530

    column data search

--- newindex.js
+++ newindex.js
@@ -1,13 +1,13 @@
 let tableDOM = document.querySelector('.table');
 
 let searchDOM = document.querySelector('.txt');
-searchDOM.addEventListener('keyup',getData);
+searchDOM.addEventListener('keyup', getData);
 
-function getData(){
-  for(let row of tableDOM.children[0].children[1].children){
-    if(row.innerText.toUpperCase().includes(searchDOM.value.toUpperCase())){
+function getData() {
+  for (let row of tableDOM.children[0].children[1].children) {
+    if (row.innerText.toUpperCase().includes(searchDOM.value.toUpperCase())) {
       row.classList.remove('hide');
-    } else{
+    } else {
       row.classList.add('hide');
     }
   }
@@ -60,26 +60,26 @@ const columns = ['id', 'name', 'phone', 'email', 'username', 'website'];
 
 
 fetch('https://jsonplaceholder.typicode.com/users')
-	.then((response) => response.json())
-	.then((json) => {
-		let table = `<table align = "center" border="1"><thead><tr>`;
+  .then((response) => response.json())
+  .then((json) => {
+    let table = `<table align = "center" border="1"><thead><tr>`;
     for (let i = 0; i < 2; i++) {
-      for(let j=0; j<columns.length; j++){
-        if(i===0){
+      for (let j = 0; j < columns.length; j++) {
+        if (i === 0) {
           table += `<td><input type="" class="txtcolumn" placeholder="Search in column"></td>`;
         }
-        else{
+        else {
           table += `<th>${columns[j].toUpperCase()}</th>`;
         }
       }
       table += `</tr>`;
-		}
+    }
     table += `</tr>`
-		for (let i = 0; i < columns.length; i++) {
-		}
-		table += `</thead><tbody>`;
-		for (let i = 0; i < json.length; i++) {
-			table += `
+    for (let i = 0; i < columns.length; i++) {
+    }
+    table += `</thead><tbody>`;
+    for (let i = 0; i < json.length; i++) {
+      table += `
         <tr>
           <td>${json[i].id}</td>
           <td>${json[i].name}</td>
@@ -88,11 +88,11 @@ fetch('https://jsonplaceholder.typicode.com/users')
           <td>${json[i].username}</td>
           <td>${json[i].website}</td>
         </tr>`;
-		}
+    }
     table += `</tbody></table>`;
-		tableDOM.innerHTML = table;
+    tableDOM.innerHTML = table;
 
-    
+
 
     // function getColumnData(){
     //   for(let column of tableDOM.children[0].children[1].children){
@@ -103,14 +103,18 @@ fetch('https://jsonplaceholder.typicode.com/users')
     //     }
     //   }
     // }
-    
+
     let searchColumnDOM = document.getElementsByClassName('txtcolumn');
-    for(let key of searchColumnDOM){
+    searchTextArr = [];
+    for (let key of searchColumnDOM) {
+      searchTextArr.push(key);
       key.addEventListener('keyup', getColumnData);
       // console.log(key);
     }
 
-    function getColumnData(){
+    console.log(searchTextArr);
+
+    function getColumnData() {
       // for(let i=0; i < tableDOM.children[0].children[1].children) {
       //   if(i.children[0].innerText.includes(searchColumnDOM[i].value)){
       //     column.classList.remove('hide');
@@ -119,22 +123,30 @@ fetch('https://jsonplaceholder.typicode.com/users')
       //   }
       // }
 
-    // if(row.innerText.toUpperCase().includes(searchDOM.value.toUpperCase())){
-
-      
-      for(let column of tableDOM.children[0].children[1].children){
-        for(let value = 0; value < column.children.length; value++){
-          if(column.children[value].innerText.includes(searchColumnDOM[value].value)){
-            column.classList.remove('hide');
-          } else {
-            column.classList.add('hide');
-          }
-
+      for (let column of tableDOM.children[0].children[1].children) {
+        if (column.children[3].innerText.toUpperCase().includes(searchTextArr[3].value.toUpperCase())) {
+          column.classList.remove('hide');
+        } else {
+          column.classList.add('hide');
         }
-        // console.log(column.children.length);
-        // console.log(column.children[0].innerText);
-        // console.log(searchColumnDOM[0].value);
-
       }
+
+      // for(let column of tableDOM.children[0].children[1].children){
+      //   console.log(searchColumnDOM[0].value);
+      //   for(let i=0; i < column.children.length; i++){
+      //     for(let j=0; j < column.children[i].length; j++){
+      //       console.log(column.children[i].innerText);
+
+      //       if(column.children[i].innerText.includes(searchColumnDOM[i].value)){
+      //         console.log("If block");
+      //         column.classList.remove('hide');
+      //       } else {
+      //         console.log("Else block");
+      //         column.classList.add('hide');
+      //       }
+      //     }
+      //   }
+      // }
+
     }
-	});
+  });
